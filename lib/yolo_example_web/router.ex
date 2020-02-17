c66558d1e6b5ff9a170bf2b7b4df868ce5937c9e
Kel Cecil <kel@mileout.com>
2020-02-17 10:25:12 -0500

    Fix resources typo

--- lib/yolo_example_web/router.ex
+++ lib/yolo_example_web/router.ex
@@ -17,6 +17,7 @@ defmodule YoloExampleWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources("/uploads", UploadController, only: [:new, :create])
   end
 
   # Other scopes may use custom stacks.
